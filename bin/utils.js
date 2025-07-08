4fd85db6764beb425d8ea953e64cd41f8429dbd4
chengpeiquan <chengpeiquan@chengpeiquan.com>
2025-07-08 23:47:32 +0800

    fix(svg): force stroke rendering for dash and similar line-only icons to fix color display issue

--- bin/utils.js
+++ bin/utils.js
@@ -1,15 +1,24 @@
 const upperCamelCase = require('uppercamelcase')
 
+// Well, some icons are just lines, but they follow the fill logic.
+const forceStroke = (name = '') => name.toLowerCase() === 'dash'
+
 const parseName = (name, defaultStyle) => {
   const nameSlices = name.split('-')
   const style = nameSlices[nameSlices.length - 1]
+
+  const finalStyle = (() => {
+    if (forceStroke(name)) return 'stroke'
+    return style === 'fill' || style === 'stroke' ? style : defaultStyle
+  })()
+
   return {
     name,
     componentName: upperCamelCase(name),
-    style: style==='fill' || style==='stroke' ? style : defaultStyle
+    style: finalStyle,
   }
 }
 
 module.exports = {
-  parseName
-};
+  parseName,
+}
