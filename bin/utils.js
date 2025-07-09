16e1c2b71ab111950ed24f7e6083573a24628002
chengpeiquan <chengpeiquan@chengpeiquan.com>
2025-07-09 10:09:09 +0800

    chore(svg): standardize the design of icons and remove special logic

--- bin/utils.js
+++ bin/utils.js
@@ -1,16 +1,11 @@
 const upperCamelCase = require('uppercamelcase')
 
-// Well, some icons are just lines, but they follow the fill logic.
-const forceStroke = (name = '') => name.toLowerCase() === 'dash'
-
 const parseName = (name, defaultStyle) => {
   const nameSlices = name.split('_')
   const style = nameSlices[nameSlices.length - 1]
 
-  const finalStyle = (() => {
-    if (forceStroke(name)) return 'stroke'
-    return style === 'fill' || style === 'stroke' ? style : defaultStyle
-  })()
+  const finalStyle =
+    style === 'fill' || style === 'stroke' ? style : defaultStyle
 
   return {
     name,
--- src/icons/dash.js
+++ src/icons/dash.js
@@ -9,14 +9,10 @@ const Dash = props => {
       width="1em"
       height="1em"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke={color}
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      fill={color}
       {...otherProps}
     >
-      <path d="M3 12h18"></path>
+      <path d="M21 11a1 1 0 110 2H3a1 1 0 110-2h18z"></path>
     </svg>
   );
 };
